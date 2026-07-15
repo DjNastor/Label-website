6aa25e2a59ac4cd2f8f42ecb2a55439965f162f9
Phushi plan music <31492366+DjNastor@users.noreply.github.com>
2026-07-16 01:29:09 +0200

    update changes in new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
-
 export default {
   content: [
-  './index.html',
-  './src/**/*.{js,ts,jsx,tsx}'
-],
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {
+      fontFamily: {
+        maputo: ['Maputo', 'sans-serif'],
+        syne: ['Syne', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        'league-spartan': ['"League Spartan"', 'sans-serif'],
+      },
       colors: {
         navy: {
           DEFAULT: '#0A0F1E',
@@ -18,12 +23,10 @@ export default {
           light: '#FDE68A'
         }
       },
-      fontFamily: {
-        syne: ['Syne', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'],
-        'league-spartan': ['"League Spartan"', 'sans-serif'],
+      dropShadow: {
+        'text': '0 2px 2px rgba(0, 0, 0, 0.5)',
       }
-    },
+    }
   },
   plugins: [],
 }
